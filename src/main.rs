d9f48511718fa82b782626c91521a723a7f32cdb
Giuliano Bellini s294739 <s294739@studenti.polito.it>
2023-12-29 12:07:42 +0100

    added println to debug

--- src/main.rs
+++ src/main.rs
@@ -64,7 +64,9 @@ fn main() {
 
     loop {
         // read a packet from the kernel
+        println!("before tun read");
         let num_bytes_out = dev.read(&mut buf_out).unwrap_or(0);
+        println!("after tun read");
         // send the packet to the socket
         if num_bytes_out > 0 {
             socket.send_to(&buf_out[0..num_bytes_out], &dst_socket_address).unwrap_or(0);
