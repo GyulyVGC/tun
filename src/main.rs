2794b65723d87e3fd08f5a8fb8a150eac3042d2e
Giuliano Bellini s294739 <s294739@studenti.polito.it>
2023-12-29 15:50:35 +0100

    some more improvements

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,61 @@
 use once_cell::sync::Lazy;
 use std::collections::HashMap;
 use std::io::{Read, Write};
-use std::net::UdpSocket;
+use std::net::{IpAddr, SocketAddr, UdpSocket};
+use std::str::FromStr;
 use std::{env, process};
 
 const PORT: u16 = 9999;
 
-pub static ETHERNET_TO_TUN_ADDRESSES: Lazy<HashMap<String, String>> = Lazy::new(|| {
-    HashMap::from([
-        (String::from("192.168.1.162"), String::from("10.0.0.1")),
-        (String::from("192.168.1.144"), String::from("10.0.0.2")),
-    ])
+static ETHERNET_TUN_TUPLES: Lazy<Vec<(IpAddr, IpAddr)>> = Lazy::new(|| {
+    vec![
+        // VM 999
+        (
+            IpAddr::from([192, 168, 1, 162]),
+            IpAddr::from([10, 0, 0, 1]),
+        ),
+        // VM 997
+        (
+            IpAddr::from([192, 168, 1, 144]),
+            IpAddr::from([10, 0, 0, 2]),
+        ),
+    ]
+});
+
+pub static ETHERNET_TO_TUN: Lazy<HashMap<IpAddr, IpAddr>> = Lazy::new(|| {
+    let mut map = HashMap::new();
+    for (ethernet, tun) in ETHERNET_TUN_TUPLES.iter() {
+        assert!(map.insert(ethernet.to_owned(), tun.to_owned()).is_none());
+    }
+    map
+});
+
+pub static TUN_TO_ETHERNET: Lazy<HashMap<IpAddr, IpAddr>> = Lazy::new(|| {
+    let mut map = HashMap::new();
+    for (ethernet, tun) in ETHERNET_TUN_TUPLES.iter() {
+        assert!(map.insert(tun.to_owned(), ethernet.to_owned()).is_none());
+    }
+    map
 });
 
 fn main() {
     let mut args = env::args().skip(1);
-    let Some(src_eth_address) = args.next() else {
+    let Some(src_eth_string) = args.next() else {
         eprintln!("Expected CLI arguments: <src_eth_address> <dst_eth_address>");
         process::exit(1);
     };
-    let Some(dst_eth_address) = args.next() else {
-            eprintln!("Expected CLI arguments: <src_eth_address> <dst_eth_address>");
-            process::exit(1);
+    let Some(dst_eth_string) = args.next() else {
+        eprintln!("Expected CLI arguments: <src_eth_address> <dst_eth_address>");
+        process::exit(1);
     };
-    let src_socket_address = format!("{src_eth_address}:{PORT}");
-    let dst_socket_address = format!("{dst_eth_address}:{PORT}");
+    let src_eth_address = IpAddr::from_str(&src_eth_string).unwrap();
+    let dst_eth_address = IpAddr::from_str(&dst_eth_string).unwrap();
+    let src_socket_address = SocketAddr::new(src_eth_address, PORT);
+    let dst_socket_address = SocketAddr::new(dst_eth_address, PORT);
 
     let mut config = tun::Configuration::default();
     config
-        .address(ETHERNET_TO_TUN_ADDRESSES.get(&src_eth_address).unwrap())
+        .address(ETHERNET_TO_TUN.get(&src_eth_address).unwrap())
         .netmask((255, 255, 255, 0))
         .up();
 
@@ -49,7 +76,7 @@ fn main() {
         // send the packet to the socket
         if num_bytes_out > 0 {
             socket
-                .send_to(&buf_out[0..num_bytes_out], &dst_socket_address)
+                .send_to(&buf_out[0..num_bytes_out], dst_socket_address)
                 .unwrap_or(0);
             println!(
                 "OUT to {}\n\t{:?}\n",
