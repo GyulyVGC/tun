d898008e8bc18da37ba352cbbf4976b629a93bd3
Giuliano Bellini s294739 <s294739@studenti.polito.it>
2023-12-29 22:58:53 +0100

    assing a name to TUN on linux

--- src/main.rs
+++ src/main.rs
@@ -32,32 +32,17 @@ async fn main() -> io::Result<()> {
     let dst_socket_address = SocketAddr::new(dst_eth_address, PORT);
 
     let mut config = tun::Configuration::default();
+    #[cfg(not(target_os = "macos"))]
+    config.name(tun_name(&src_eth_address));
     config
         .address(ETHERNET_TO_TUN.get(&src_eth_address).unwrap())
         .netmask((255, 255, 255, 0))
         .up();
 
-    // to work between linux and macos/other, it's needed to add 4 bytes in front of packets
-    // that's because linux TUNs use link_type=raw, while other use link_type=null/loopback
-    // #[cfg(target_os = "linux")]
-    // config.platform(|config| {
-    //     config.packet_information(true);
-    // });
-
     let (device_out, device_in) = tun::create(&config).unwrap().split();
 
-    // to work on macOS, the route must be setup manually
     #[cfg(target_os = "macos")]
-    Command::new("route")
-        .args([
-            "-n",
-            "add",
-            "-net",
-            "10.0.0.0/24",
-            &ETHERNET_TO_TUN.get(&src_eth_address).unwrap().to_string(),
-        ])
-        .spawn()
-        .unwrap();
+    configure_routing_macos(&src_eth_address);
 
     let socket = UdpSocket::bind(src_socket_address).await?;
     let socket_in = Arc::new(socket);
@@ -70,4 +55,27 @@ async fn main() -> io::Result<()> {
     send(device_out, socket_out, dst_socket_address).await?;
 
     Ok(())
+}
+
+/// Returns a name in the form 'tun-nullnet-x' where x is the host part of the TUN's ip
+/// Example: the TUN with address 10.0.0.1 will be called nullnet-1 (supposing netmask /24)
+#[cfg(not(target_os = "macos"))]
+fn tun_name(src_eth_address: &IpAddr) -> String {
+    let tun_ip = ETHERNET_TO_TUN.get(src_eth_address).unwrap().to_string();
+    let num = tun_ip.split(".").last().unwrap();
+    format!("utun-nullnet-{}", num)
+}
+
+/// To work on macOS, the route must be setup manually (after TUN creation!)
+fn configure_routing_macos(src_eth_address: &IpAddr) {
+    Command::new("route")
+        .args([
+            "-n",
+            "add",
+            "-net",
+            "10.0.0.0/24",
+            &ETHERNET_TO_TUN.get(src_eth_address).unwrap().to_string(),
+        ])
+        .spawn()
+        .unwrap();
 }
