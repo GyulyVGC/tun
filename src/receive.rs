8a734364295b0070b33a28ece89f6c76d11020ff
Giuliano Bellini s294739 <s294739@studenti.polito.it>
2024-01-03 23:41:57 +0100

    made the TUN device async (tokio::io is now used for reads and writes)

--- src/receive.rs
+++ src/receive.rs
@@ -1,12 +1,12 @@
 use crate::socket_frame::SocketFrame;
-use std::io::Write;
 use std::net::SocketAddr;
 use std::str::FromStr;
 use std::sync::Arc;
+use tokio::io::{AsyncWriteExt, WriteHalf};
 use tokio::net::UdpSocket;
-use tun::platform::posix::Writer;
+use tun::AsyncDevice;
 
-pub async fn receive(mut device: Writer, socket: Arc<UdpSocket>) {
+pub async fn receive(mut device: WriteHalf<AsyncDevice>, socket: Arc<UdpSocket>) {
     let mut socket_frame = SocketFrame::new();
     loop {
         // wait until there is an incoming packet on the socket (packets on the socket are raw IP)
@@ -25,7 +25,7 @@ pub async fn receive(mut device: Writer, socket: Arc<UdpSocket>) {
             let os_buf = socket_frame.to_os_buf();
 
             #[allow(clippy::needless_borrow)]
-            device.write_all(&os_buf).unwrap_or(());
+            device.write_all(&os_buf).await.unwrap_or(());
         }
     }
 }
