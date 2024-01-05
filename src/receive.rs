c9c5f2799e81126a634162ed394a9bcb5fee8a26
Giuliano Bellini s294739 <s294739@studenti.polito.it>
2024-01-06 00:25:23 +0100

    add prints to measure elapsed times

--- src/receive.rs
+++ src/receive.rs
@@ -2,6 +2,7 @@ use crate::socket_frame::SocketFrame;
 use std::net::SocketAddr;
 use std::str::FromStr;
 use std::sync::Arc;
+use std::time::Instant;
 use tokio::io::{AsyncWriteExt, WriteHalf};
 use tokio::net::UdpSocket;
 use tun::AsyncDevice;
@@ -9,25 +10,19 @@ use tun::AsyncDevice;
 pub async fn receive(mut device: WriteHalf<AsyncDevice>, socket: Arc<UdpSocket>) {
     let mut socket_frame = SocketFrame::new();
     loop {
+        let inst = Instant::now();
         // wait until there is an incoming packet on the socket (packets on the socket are raw IP)
         (socket_frame.actual_bytes, _) = socket
             .recv_from(&mut socket_frame.frame)
             .await
-            .unwrap_or_else(|_| {
-                println!("hey hey");
-                (0, SocketAddr::from_str("0.0.0.0:0").unwrap())
-            });
+            .unwrap_or_else(|_| (0, SocketAddr::from_str("0.0.0.0:0").unwrap()));
 
         if socket_frame.actual_bytes > 0 {
             // write packet to the kernel
             let os_buf = socket_frame.to_os_buf();
             #[allow(clippy::needless_borrow)]
-            let x = device.write(&os_buf).await.unwrap_or(0);
-            if x == 0 {
-                println!("cow cow");
-            }
-        } else {
-            println!("rx hoy hoy");
+            device.write_all(&os_buf).await.unwrap_or(());
         }
+        println!("RX {}", inst.elapsed().as_micros());
     }
 }
--- src/send.rs
+++ src/send.rs
@@ -2,6 +2,7 @@ use crate::os_frame::OsFrame;
 use crate::peers::TUN_TO_SOCKET;
 use std::net::SocketAddr;
 use std::sync::Arc;
+use std::time::Instant;
 use tokio::io::{AsyncReadExt, ReadHalf};
 use tokio::net::UdpSocket;
 use tun::AsyncDevice;
@@ -9,25 +10,22 @@ use tun::AsyncDevice;
 pub async fn send(mut device: ReadHalf<AsyncDevice>, socket: Arc<UdpSocket>) {
     let mut os_frame = OsFrame::new();
     loop {
+        let inst = Instant::now();
         // wait until there is a packet outgoing from kernel
         os_frame.actual_bytes = device.read(&mut os_frame.frame).await.unwrap_or(0);
 
         // send the packet to the socket
         let socket_buf = os_frame.to_socket_buf();
         let Some(dst_socket) = get_dst_socket(socket_buf) else {
-            println!("sock sock");
             continue;
         };
-        let x = socket.send_to(socket_buf, dst_socket).await.unwrap_or(0);
-        if x == 0 {
-            println!("mmh");
-        }
+        socket.send_to(socket_buf, dst_socket).await.unwrap_or(0);
+        println!("TX {}", inst.elapsed().as_micros());
     }
 }
 
 fn get_dst_socket(socket_buf: &[u8]) -> Option<&SocketAddr> {
     if socket_buf.len() < 20 {
-        println!("tx hoy hoy");
         None
     } else {
         TUN_TO_SOCKET.get(&socket_buf[16..20])
