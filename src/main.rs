b7d18c3e5208a3175d4212378bda16294f5f4751
Giuliano Bellini s294739 <s294739@studenti.polito.it>
2024-01-05 11:27:33 +0100

    back to Layer 3

--- src/main.rs
+++ src/main.rs
@@ -36,7 +36,6 @@ async fn main() {
     let mut config = Configuration::default();
     set_tun_name(&src_socket_ip, &mut config);
     config
-        .layer(Layer::L2)
         .mtu(i32::try_from(MTU).unwrap())
         .address(
             ETHERNET_TO_TUN
