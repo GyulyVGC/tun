dfe3ab1047fe7ae54d2661f5c7126183ce968f4b
Giuliano Bellini s294739 <s294739@studenti.polito.it>
2024-01-12 14:35:22 +0100

    feat: send destination unreachable in case of rejected incoming packets

--- src/main.rs
+++ src/main.rs
@@ -37,17 +37,17 @@ fn main() {
         firewall_path,
     } = Args::parse();
 
+    let tun_ip = ETHERNET_TO_TUN
+        .get(&source)
+        .expect("Address is not in the list of peers");
+
     let src_socket = SocketAddr::new(source, PORT);
 
     let mut config = Configuration::default();
-    set_tun_name(&source, &mut config);
+    set_tun_name(tun_ip, &mut config);
     config
         .mtu(i32::try_from(mtu).unwrap())
-        .address(
-            ETHERNET_TO_TUN
-                .get(&source)
-                .expect("Address is not in the list of peers"),
-        )
+        .address(tun_ip)
         .netmask((255, 255, 255, 0))
         .up();
 
@@ -55,7 +55,7 @@ fn main() {
         .expect("Failed to create TUN device")
         .split();
 
-    configure_routing(&source);
+    configure_routing(tun_ip);
 
     let socket = UdpSocket::bind(src_socket).expect("Failed to bind socket");
     let socket_in = Arc::new(socket);
@@ -69,7 +69,7 @@ fn main() {
     let firewall_w = firewall_r1.clone();
 
     thread::spawn(move || {
-        receive(device_in, &socket_in, &firewall_r1);
+        receive(device_in, &socket_in, &firewall_r1, tun_ip);
     });
 
     thread::spawn(move || {
@@ -81,32 +81,19 @@ fn main() {
 
 /// Returns a name in the form 'nullnetX' where X is the host part of the TUN's ip (doesn't work on macOS)
 /// Example: the TUN with address 10.0.0.1 will be called nullnet1 (this supposes netmask /24)
-fn set_tun_name(_source: &IpAddr, _config: &mut Configuration) {
+fn set_tun_name(_tun_ip: &IpAddr, _config: &mut Configuration) {
     #[cfg(not(target_os = "macos"))]
     {
-        let tun_ip = ETHERNET_TO_TUN
-            .get(_source)
-            .expect("Address is not in the list of peers")
-            .to_string();
-        let num = tun_ip.split('.').last().unwrap();
+        let num = tun_ip.to_string().split('.').last().unwrap();
         _config.name(format!("nullnet{num}"));
     }
 }
 
 /// To work on macOS, the route must be setup manually (after TUN creation!)
-fn configure_routing(_source: &IpAddr) {
+fn configure_routing(_tun_ip: &IpAddr) {
     #[cfg(target_os = "macos")]
     process::Command::new("route")
-        .args([
-            "-n",
-            "add",
-            "-net",
-            "10.0.0.0/24",
-            &ETHERNET_TO_TUN
-                .get(_source)
-                .expect("Address is not in the list of peers")
-                .to_string(),
-        ])
+        .args(["-n", "add", "-net", "10.0.0.0/24", &_tun_ip.to_string()])
         .spawn()
         .expect("Failed to configure routing");
 }
--- src/receive.rs
+++ src/receive.rs
@@ -1,13 +1,19 @@
+use crate::peers::TUN_TO_SOCKET;
 use crate::socket_frame::SocketFrame;
 use nullnet_firewall::{Firewall, FirewallAction, FirewallDirection};
 use std::io::Write;
-use std::net::SocketAddr;
 use std::net::UdpSocket;
+use std::net::{IpAddr, SocketAddr};
 use std::str::FromStr;
 use std::sync::{Arc, RwLock};
 use tun::platform::posix::Writer;
 
-pub fn receive(mut device: Writer, socket: &Arc<UdpSocket>, firewall: &Arc<RwLock<Firewall>>) {
+pub fn receive(
+    mut device: Writer,
+    socket: &Arc<UdpSocket>,
+    firewall: &Arc<RwLock<Firewall>>,
+    tun_ip: &IpAddr,
+) {
     let mut socket_frame = SocketFrame::new();
     loop {
         // wait until there is an incoming packet on the socket (packets on the socket are raw IP)
@@ -27,8 +33,100 @@ pub fn receive(mut device: Writer, socket: &Arc<UdpSocket>, firewall: &Arc<RwLoc
                     #[allow(clippy::needless_borrow)]
                     device.write_all(&os_buf).unwrap_or(());
                 }
-                FirewallAction::DENY | FirewallAction::REJECT => {}
+                FirewallAction::REJECT => {
+                    send_destination_unreachable(socket_frame.actual_frame(), tun_ip, socket)
+                }
+                FirewallAction::DENY => {}
             }
         }
     }
+}
+
+fn send_destination_unreachable(packet: &[u8], tun_ip: &IpAddr, socket: &Arc<UdpSocket>) {
+    if packet.len() < 28 {
+        return;
+    }
+
+    let rejected_sender_ip = &packet[12..16];
+    let Some(dest_socket) = TUN_TO_SOCKET.get(rejected_sender_ip) else {
+        return;
+    };
+
+    let mut pkt_response = [
+        // ipv4 header
+        0x45, 0x00, // version, header length, congestion
+        0x00, 0x00, // length (will be set later)
+        0x00, 0x00, 0x00, 0x00, // identification, fragmentation
+        0x40, 0x01, // ttl and protocol
+        0x00, 0x00, // header checksum (will be set later)
+        0x00, 0x00, 0x00, 0x00, // source (will be set later)
+        0x00, 0x00, 0x00, 0x00, // dest (will be set later)
+        // icmp header
+        0x03, 0x01, // destination host unreachable
+        0x00, 0x00, // checksum (will be set later)
+        0x00, 0x00, 0x00,
+        0x00, // unused
+              // the original ip header and the first 64 bits
+              // of the original datagram will be included here
+    ];
+
+    // length
+    pkt_response[2] = 0x00; // 56 bytes (0x0038)
+    pkt_response[3] = 0x38;
+
+    // source
+    let IpAddr::V4(tun_ip_v4) = tun_ip else {
+        panic!("IPv6 not supported yet as TUN address...");
+    };
+    pkt_response[12..16].clone_from_slice(&tun_ip_v4.octets()); // my IP
+
+    // dest
+    pkt_response[16..20].clone_from_slice(&packet[12..16]); // sender of the rejected packet
+
+    // ip header checksum
+    let ip_checksum = calc_ipv4_checksum(&pkt_response[..20]);
+    pkt_response[10] = (ip_checksum >> 8) as u8; // calculated checksum is little-endian; checksum field is big-endian
+    pkt_response[11] = (ip_checksum & 0xff) as u8; // calculated checksum is little-endian; checksum field is big-endian
+
+    // rest of the packet: original IP header and first 8 bytes of data
+    let pkt_response_final = &mut [&pkt_response[..], &packet[..28]].concat()[..];
+
+    // icmp checksum
+    let icmp_checksum = calc_icmp_checksum(&pkt_response_final[20..]);
+    pkt_response_final[22] = (icmp_checksum >> 8) as u8; // calculated checksum is little-endian; checksum field is big-endian
+    pkt_response_final[23] = (icmp_checksum & 0xff) as u8; // calculated checksum is little-endian; checksum field is big-endian
+
+    socket.send_to(pkt_response_final, dest_socket).unwrap_or(0);
+}
+
+fn calc_ipv4_checksum(ipv4_header: &[u8]) -> u16 {
+    assert_eq!(ipv4_header.len() % 2, 0);
+    let mut checksum = 0;
+    for i in 0..ipv4_header.len() / 2 {
+        if i == 5 {
+            // Assume checksum field is set to 0
+            continue;
+        }
+        checksum += (u32::from(ipv4_header[i * 2]) << 8) + u32::from(ipv4_header[i * 2 + 1]);
+        if checksum > 0xffff {
+            checksum = (checksum & 0xffff) + 1;
+        }
+    }
+    !u16::try_from(checksum).unwrap()
+}
+
+fn calc_icmp_checksum(icmp_data: &[u8]) -> u16 {
+    assert_eq!(icmp_data.len() % 2, 0);
+    let mut checksum = 0;
+    for i in 0..icmp_data.len() / 2 {
+        if i == 1 {
+            // Assume checksum field is set to 0
+            continue;
+        }
+        checksum += (u32::from(icmp_data[i * 2]) << 8) + u32::from(icmp_data[i * 2 + 1]);
+        if checksum > 0xffff {
+            checksum = (checksum & 0xffff) + 1;
+        }
+    }
+    !u16::try_from(checksum).unwrap()
 }
