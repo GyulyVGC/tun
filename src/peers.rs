7ea9e77e142702b18c144972ddd78a7babef7e63
Giuliano Bellini s294739 <s294739@studenti.polito.it>
2024-01-23 12:23:17 +0100

    add VM 993 in the list of peers

--- src/peers.rs
+++ src/peers.rs
@@ -6,12 +6,14 @@ use std::net::SocketAddr;
 
 static ETHERNET_TUN_TUPLES: Lazy<Vec<([u8; 4], [u8; 4])>> = Lazy::new(|| {
     vec![
-        // Proxmox VM 999
+        // Proxmox VM 999 (Debian)
         ([192, 168, 1, 162], [10, 0, 0, 1]),
-        // Proxmox VM 997
+        // Proxmox VM 997 (Debian)
         ([192, 168, 1, 144], [10, 0, 0, 2]),
         // macOS
         ([192, 168, 1, 113], [10, 0, 0, 3]),
+        // Proxmox VM 993 (Windows)
+        ([192, 168, 1, 12], [10, 0, 0, 4]),
     ]
 });
 
