2fc1d496d4a2580e226e7f3370006c54ae69d41b
Giuliano Bellini s294739 <s294739@studenti.polito.it>
2024-02-05 18:08:41 +0100

    refactor socket-related variable names

--- src/peers_discovery/discover_peers.rs
+++ src/peers_discovery/discover_peers.rs
@@ -16,25 +16,25 @@ const RETRANSMISSION_PERIOD: u64 = TTL / 4;
 const RETRIES_DELTA: u64 = 1;
 
 pub async fn discover_peers(local_eth_ip: IpAddr, tun_ip: &IpAddr) {
-    let socket_addr = SocketAddr::new(local_eth_ip, UNICAST_PORT);
-    let socket = UdpSocket::bind(socket_addr).await.unwrap(); // should not panic...
-    socket.set_broadcast(true).unwrap();
-    let socket_shared = Arc::new(socket);
+    let local_socket_addr = SocketAddr::new(local_eth_ip, UNICAST_PORT);
+    let local_socket = UdpSocket::bind(local_socket_addr).await.unwrap(); // should not panic...
+    local_socket.set_broadcast(true).unwrap();
+    let local_socket_shared = Arc::new(local_socket);
 
     tokio::spawn(async move {
         listen_broadcast().await; // this will also call hello_unicast...
     });
     // also listen_unicast will be spawned here...
 
-    hello_broadcast(socket_shared, tun_ip).await;
+    hello_broadcast(local_socket_shared, tun_ip).await;
 }
 
 /// Listens to broadcast messages. TODO!
 async fn listen_broadcast() {
     let mut msg = [0; 1024];
-    let socket = UdpSocket::bind(socket_broadcast_listen()).await.unwrap();
+    let listen_broadcast_socket = UdpSocket::bind(listen_broadcast_socket()).await.unwrap();
     loop {
-        let (msg_len, from) = socket
+        let (msg_len, from) = listen_broadcast_socket
             .recv_from(&mut msg)
             .await
             .unwrap_or_else(|_| (0, SocketAddr::from_str("0.0.0.0:0").unwrap()));
@@ -46,13 +46,16 @@ async fn listen_broadcast() {
 }
 
 /// Periodically sends out messages to let other peers know that this device is up.
-async fn hello_broadcast(socket: Arc<UdpSocket>, tun_ip: &IpAddr) {
-    let dest = socket_broadcast_hello();
+async fn hello_broadcast(local_socket: Arc<UdpSocket>, tun_ip: &IpAddr) {
+    let hello_broadcast_socket = hello_broadcast_socket();
     let tun_ip_string = tun_ip.to_string();
     let msg = tun_ip_string.as_bytes();
     loop {
         for _ in 0..RETRIES {
-            let _msg_len = socket.send_to(msg, dest).await.unwrap_or(0);
+            let _msg_len = local_socket
+                .send_to(msg, hello_broadcast_socket)
+                .await
+                .unwrap_or(0);
             tokio::time::sleep(Duration::from_secs(RETRIES_DELTA)).await;
         }
         tokio::time::sleep(Duration::from_secs(RETRANSMISSION_PERIOD)).await;
@@ -60,11 +63,11 @@ async fn hello_broadcast(socket: Arc<UdpSocket>, tun_ip: &IpAddr) {
 }
 
 /// Returns the broadcast socket destination of greeting messages.
-fn socket_broadcast_hello() -> SocketAddr {
+fn hello_broadcast_socket() -> SocketAddr {
     SocketAddr::new(IpAddr::V4(Ipv4Addr::BROADCAST), BROADCAST_PORT)
 }
 
 /// Returns the socket used to listen to greeting messages.
-fn socket_broadcast_listen() -> SocketAddr {
+fn listen_broadcast_socket() -> SocketAddr {
     SocketAddr::new(IpAddr::V4(Ipv4Addr::UNSPECIFIED), BROADCAST_PORT)
 }
