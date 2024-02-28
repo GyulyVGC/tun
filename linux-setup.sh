a0f2ac392fbd793258eff797db1aab1769129536
Giuliano Bellini s294739 <s294739@studenti.polito.it>
2024-02-28 12:10:55 +0100

    attempt fix linux-setup.sh

--- linux-setup.sh
+++ linux-setup.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
 
 # Update nullnet.service file
-cp nullnet.service /etc/systemd/system/
-systemctl enable nullnet
-
-# Build latest version of the executable
-git checkout main
-git pull
-cargo b --release
-
-# Reboot
+cp nullnet.service /etc/systemd/system/ && \
+systemctl enable nullnet && \
+git checkout main && \
+git pull && \
+cargo b --release && \
 reboot
