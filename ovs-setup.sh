701cf34c8c33a07193cc88b497ba5ee58e4acfa6
GyulyVGC <gyulyvgc99@gmail.com>
2025-12-03 17:42:00 +0100

    improve ovs-setup.sh (use the built-in switching logic)

--- ovs-setup.sh
+++ ovs-setup.sh
@@ -2,7 +2,13 @@
 
 if [ $# -eq 0 ]
   then
-    echo "No IP address supplied for the veth10p interface"
+    echo "No IP address/subnet supplied for the veth10p interface"
+    exit 1
+fi
+
+if [ $# -gt 1 ]
+  then
+    echo "Too many arguments supplied (only supply the IP address/subnet for the veth10p interface)"
     exit 1
 fi
 
@@ -12,10 +18,10 @@ sudo ovs-vsctl add-br br0
 sudo ip link set ovs-system up
 sudo ip link set br0 up
 
-# nullnet0 for VLAN 10 (trunk port)
+# set nullnet0 as a trunk port
 sudo ovs-vsctl add-port br0 nullnet0
 
-# veth pair for VLAN 10 (access port)
+# set veth pair as access port for VLAN 10
 sudo ip link del veth10 >/dev/null 2>&1
 sudo ip link add veth10 type veth peer name veth10p
 sudo ip link set veth10 up
@@ -23,11 +29,17 @@ sudo ip link set veth10p up
 sudo ip addr add "$1" dev veth10p
 sudo ovs-vsctl add-port br0 veth10 tag=10
 
-# TODO: populate ARP table for veth10p
-
 # delete existing OpenFlow rules
-sudo ovs-ofctl -O OpenFlow13 del-flows br0
+sudo ovs-ofctl del-flows br0
+# use the built-in switching logic
+sudo ovs-ofctl add-flow br0 "priority=0,actions=normal"
+
+
+# ----------------------------------------------------------------------------------------------------------------------
+# TODO: populate ARP table for veth10p
+# ----------------------------------------------------------------------------------------------------------------------
 # OpenFlow rule: veth10 --> nullnet0 with VLAN 10 tagging
-sudo ovs-ofctl -O OpenFlow13 add-flow br0 "in_port=2,actions=push_vlan:0x8100,set_vlan_vid:10,output:1"
+#sudo ovs-ofctl -O OpenFlow13 add-flow br0 "in_port=2,actions=push_vlan:0x8100,set_vlan_vid:10,output:1"
 # OpenFlow rule: nullnet0 --> veth10 with VLAN 10 untagging
-sudo ovs-ofctl -O OpenFlow13 add-flow br0 "in_port=1,dl_vlan=10,actions=pop_vlan,output:2"
+#sudo ovs-ofctl -O OpenFlow13 add-flow br0 "in_port=1,dl_vlan=10,actions=pop_vlan,output:2"
+# ----------------------------------------------------------------------------------------------------------------------
