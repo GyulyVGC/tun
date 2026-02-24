7b94575a50aeafd387a75d5442b17fe0294e83c6
GyulyVGC <gyulyvgc99@gmail.com>
2026-02-24 13:12:00 +0100

    update vxlan-setup.sh to include vlan_id CLI argument

--- vxlan-setup.sh
+++ vxlan-setup.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
 
 # Read CLI arguments:
-if [ "$#" -ne 6 ]; then
-    echo "Usage: $0 <ns_name> <ns_net> <br_name> <br_net> <local_ip> <remote_ip>"
-    echo "Example: $0 red 10.0.0.1/24 bridge-main 10.0.0.2/24 192.168.1.102 192.168.1.104"
+if [ "$#" -ne 7 ]; then
+    echo "Usage: $0 <vxlan_id> <ns_name> <ns_net> <br_name> <br_net> <local_ip> <remote_ip>"
+    echo "Example: $0 100 red 10.0.0.1/24 bridge-main 10.0.0.2/24 192.168.1.102 192.168.1.104"
     exit 1
 fi
 
-NS_NAME=$1
-NS_NET=$2
-BR_NAME=$3
-BR_NET=$4
-LOCAL_IP=$5
-REMOTE_IP=$6
+VXLAN_ID=$1
+NS_NAME=$2
+NS_NET=$3
+BR_NAME=$4
+BR_NET=$5
+LOCAL_IP=$6
+REMOTE_IP=$7
 
 BR_IP=$(echo $BR_NET | cut -d'/' -f1)
 
@@ -32,7 +33,7 @@ sudo ip link set $BR_NAME up
 sudo ip netns exec $NS_NAME ip route add default via $BR_IP
 
 # Create the VXLAN tunnel using your physical IP and interface:
-sudo ip link add vxlan-$NS_NAME type vxlan id 100 local $LOCAL_IP remote $REMOTE_IP dev ens18
+sudo ip link add vxlan-$NS_NAME type vxlan id $VXLAN_ID local $LOCAL_IP remote $REMOTE_IP dev ens18
 
 # Attach the VXLAN to the bridge:
 sudo ip link set vxlan-$NS_NAME master $BR_NAME
